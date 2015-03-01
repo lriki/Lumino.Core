f74647f0b2c8c313c5ab0effb9cc424b059adbb5
lriki <lriki.net@gmail.com>
2015-03-01 13:26:45 +0900

    PlatformSupport

---
+++ include/Lumino/Platform/PlatformSupport.h
@@ -0,0 +1,32 @@
+﻿
+#pragma once
+
+#include "../Base/Common.h"
+
+namespace Lumino
+{
+namespace Platform
+{
+class Window;
+
+/**
+	@brief	プラットフォーム固有の機能にアクセスします。
+*/
+class PlatformSupport
+{
+public:
+#ifdef LN_WIN32
+	/**
+		@brief		ウィンドウのウィンドウハンドルを取得します。
+	*/
+	static HWND GetWindowHandle(Window* window);
+
+	/**
+		@brief		ウィンドウのデバイスコンテキストハンドルを取得します。
+	*/
+	static HDC GetWindowDC(Window* window);
+#endif
+};
+
+} // namespace Platform
+} // namespace Lumino
