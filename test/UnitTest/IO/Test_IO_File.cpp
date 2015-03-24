5247b8e08c2575cf6bef261c08b3cd00cfb11c21
lriki <lriki.net@gmail.com>
2015-03-24 19:35:58 +0900

    file

---
+++ test/UnitTest/IO/Test_IO_File.cpp
@@ -0,0 +1,14 @@
+﻿#include <TestConfig.h>
+
+class Test_IO_File : public ::testing::Test
+{
+protected:
+	virtual void SetUp() {}
+	virtual void TearDown() {}
+};
+//-----------------------------------------------------------------------------
+TEST_F(Test_IO_File, Open)
+{
+	File file1(LN_TEST_GET_FILE_PATH("TestData/test1.txt"));
+	file1.Open(FileMode_Open, FileAccess_Read);
+}
