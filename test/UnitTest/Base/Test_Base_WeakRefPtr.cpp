c06f4c2f9a64a374af0de17a5ca2f821ddc3f2e6
lriki <lriki.net@gmail.com>
2016-03-18 17:48:40 +0900

    WeakRefPtr

---
+++ test/UnitTest/Base/Test_Base_WeakRefPtr.cpp
@@ -0,0 +1,13 @@
+﻿#include <TestConfig.h>
+
+class Test_Base_WeakRefPtr : public ::testing::Test
+{
+protected:
+	virtual void SetUp() {}
+	virtual void TearDown() {}
+};
+
+//-----------------------------------------------------------------------------
+TEST_F(Test_Base_WeakRefPtr, Basic)
+{
+}
